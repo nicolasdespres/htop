adce1fe22fcaebe4f11b3bd36f17de157a0e3f07
Nicolas Despres <nicolas.despres@gmail.com>
2015-04-14 09:42:09 +0200

    autogen: Prefer glibtoolize over libtoolize.
    On Darwin libtoolize does not exists.

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,9 @@
 aclocal -I m4
 autoconf
 autoheader
-libtoolize --copy --force
+for LIBTOOLIZE in glibtoolize libtoolize
+do
+  type $LIBTOOLIZE 2>/dev/null >/dev/null && break
+done
+$LIBTOOLIZE --copy --force
 automake --add-missing --copy
-
-
